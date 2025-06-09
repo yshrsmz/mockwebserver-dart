19b959496036f1e9d6d4cc83d7f90323a3b1acb2
yshrsmz <the.phantom.bane@gmail.com>
2025-06-09 22:46:48 +0900

    update document

--- lib/src/handlers/request_handler.dart
+++ lib/src/handlers/request_handler.dart
@@ -2,9 +2,18 @@ import 'package:shelf/shelf.dart';
 
 /// An abstraction that should help us implement more handler types in the future.
 abstract interface class RequestHandler {
+  /// Count of how many times the handler has been used.
   int get usedCount;
+
+  /// Check if the handler can handle the [request].
   bool test(Request request);
+
+  /// Handle the [request] and return a response.
   Future<Response> handle(Request request);
+
+  /// Reset the handler to its initial state.
   void reset();
+
+  /// Log the [request] and [response] for debugging purposes.
   Future<void> log(Request request, Response response);
 }
